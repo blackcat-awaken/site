349edee02d0f8ee577272f29d8b61805bbe7f234
kevin930321 <kevin930321@gmail.com>
2026-06-14 21:34:15 +0800

    style: adjust reveal animation threshold to 10%

--- js/main.js
+++ js/main.js
@@ -133,7 +133,7 @@ document.addEventListener('DOMContentLoaded', () => {
         revealObserver.unobserve(entry.target);
       }
     });
-  }, { threshold: 0.15, rootMargin: '0px 0px -40px 0px' });
+  }, { threshold: 0.1, rootMargin: '0px 0px -40px 0px' });
 
   revealElements.forEach(el => revealObserver.observe(el));
 
